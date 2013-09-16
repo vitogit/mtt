f8d6c6546cf19d4d0bfb499dd563aef0b53c0f56
apuntalorapido@gmail.com <apuntalorapido@gmail.com>
2013-09-15 21:20:52 -0300

    agrego config para q anden los iframes externos

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,10 @@ module Mtt
       
       
     end
-
+    config.action_dispatch.default_headers = {
+        'X-XSS-Protection' => '1; mode=block',
+        'X-Content-Type-Options' => 'nosniff'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
